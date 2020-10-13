f0b808eb19db1eefbc26c8f9cecf70ff6e9c41c8
epwalsh <epwalsh10@gmail.com>
2020-10-12 17:53:35 -0700

    implementing padding and combining batches

--- src/data.rs
+++ src/data.rs
@@ -8,12 +8,14 @@ use std::thread;
 use tch::Tensor;
 
 use crate::common;
+use crate::tokenization::{load_tokenizer, PAD_TOKEN_ID};
 
 #[derive(Debug)]
 pub struct Batch {
     pub token_ids: Tensor,
     pub type_ids: Tensor,
-    pub gold_label: Option<Tensor>,
+    pub gold_labels: Option<Tensor>,
+    pub mask: Tensor,
 }
 
 impl Batch {
@@ -23,7 +25,8 @@ impl Batch {
             type_ids: Tensor::of_slice(type_ids)
                 .totype(tch::Kind::Int64)
                 .unsqueeze(0),
-            gold_label: gold_label.map(|label| Tensor::from(label).unsqueeze(0)),
+            gold_labels: gold_label.map(|label| Tensor::from(label).unsqueeze(0)),
+            mask: Tensor::of_slice(&vec![1; token_ids.len()]).unsqueeze(0),
         }
     }
 
@@ -33,6 +36,79 @@ impl Batch {
             &input.segment_ids,
             gold_label.map(|label| common::label2id(label)),
         )
+    }
+
+    pub fn combine(batches: &[Batch]) -> Self {
+        if batches.len() == 0 {
+            panic!("Tried to combine an empty slice of batches");
+        }
+
+        // Find max sequence length.
+        let mut max_len = 0;
+        batches
+            .iter()
+            .for_each(|b| max_len = std::cmp::max(max_len, b.token_ids.size()[1]));
+
+        let mut token_ids_tensors: Vec<Tensor> = Vec::with_capacity(batches.len());
+        let mut type_ids_tensors: Vec<Tensor> = Vec::with_capacity(batches.len());
+        let mut gold_labels_tensors: Option<Vec<&Tensor>> = match batches[0].gold_labels {
+            Some(_) => Some(Vec::with_capacity(batches.len())),
+            _ => None,
+        };
+        let mut mask_tensors: Vec<Tensor> = Vec::with_capacity(batches.len());
+
+        for b in batches {
+            let current_len = b.token_ids.size()[1];
+            let padding_needed = (max_len - current_len) as usize;
+
+            if padding_needed > 0 {
+                let token_ids = Tensor::cat(
+                    &[
+                        &b.token_ids,
+                        &Tensor::of_slice(&vec![PAD_TOKEN_ID; padding_needed]).unsqueeze(0),
+                    ],
+                    1,
+                );
+                token_ids_tensors.push(token_ids);
+
+                let type_ids = Tensor::cat(
+                    &[
+                        &b.type_ids,
+                        &Tensor::of_slice(&vec![PAD_TOKEN_ID; padding_needed]).unsqueeze(0),
+                    ],
+                    1,
+                );
+                type_ids_tensors.push(type_ids);
+
+                let mask = Tensor::cat(
+                    &[
+                        &b.mask,
+                        &Tensor::of_slice(&vec![0; padding_needed]).unsqueeze(0),
+                    ],
+                    1,
+                );
+                mask_tensors.push(mask);
+            } else {
+                token_ids_tensors.push(b.token_ids.copy());
+                type_ids_tensors.push(b.type_ids.copy());
+                mask_tensors.push(b.mask.copy());
+            }
+
+            if let Some(label_tensors) = gold_labels_tensors.as_mut() {
+                label_tensors.push(b.gold_labels.as_ref().unwrap());
+            } else {
+                if b.gold_labels.is_some() {
+                    panic!("expect batch without gold labels");
+                }
+            }
+        }
+
+        Batch {
+            token_ids: Tensor::cat(&token_ids_tensors, 0),
+            type_ids: Tensor::cat(&type_ids_tensors, 0),
+            gold_labels: gold_labels_tensors.map(|labels| Tensor::cat(&labels, 0)),
+            mask: Tensor::cat(&mask_tensors, 0),
+        }
     }
 }
 
@@ -74,8 +150,7 @@ impl Reader {
 
             let handle = thread::spawn(move || {
                 info!("Worker[{}] initializing", i);
-                let tokenizer = crate::tokenization::load_tokenizer(tokenizer_dir)
-                    .expect("failed to load tokenizer");
+                let tokenizer = load_tokenizer(tokenizer_dir).expect("failed to load tokenizer");
 
                 rx_line.iter().for_each(|line| {
                     let instance: Instance =
@@ -142,13 +217,38 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_batching() {
+        let batches = vec![
+            Batch::new(&[0, 3, 4, 2], &[0, 0, 0, 0], Some(0)),
+            Batch::new(&[0, 3, 2], &[0, 0, 0], Some(1)),
+        ];
+        let batch = Batch::combine(&batches);
+
+        assert_eq!(
+            Vec::<Vec<i64>>::from(batch.token_ids),
+            vec![[0, 3, 4, 2], [0, 3, 2, 1]]
+        );
+        assert_eq!(
+            Vec::<Vec<i64>>::from(batch.type_ids),
+            vec![[0, 0, 0, 0], [0, 0, 0, 1]]
+        );
+        assert_eq!(
+            Vec::<i64>::from(batch.gold_labels.as_ref().unwrap()),
+            vec![0, 1]
+        );
+        assert_eq!(
+            Vec::<Vec<i64>>::from(batch.mask),
+            vec![[1, 1, 1, 1], [1, 1, 1, 0]]
+        );
+    }
+
+    #[test]
     fn test_reader() {
         let mut reader = Reader::new("test_fixtures/tokenizer").unwrap();
         // Set 1 worker so the order is deterministic.
         reader.n_workers = 1;
 
-        let tokenizer = crate::tokenization::load_tokenizer(&reader.tokenizer_dir)
-            .expect("failed to load tokenizer");
+        let tokenizer = load_tokenizer(&reader.tokenizer_dir).expect("failed to load tokenizer");
 
         let mut data = reader.read("test_fixtures/snli.jsonl").unwrap();
 
@@ -156,8 +256,8 @@ mod tests {
 
         let batch = data.remove(0);
 
-        assert!(batch.gold_label.is_some());
-        let label = batch.gold_label.as_ref().unwrap();
+        assert!(batch.gold_labels.is_some());
+        let label = batch.gold_labels.as_ref().unwrap();
         assert_eq!(label.size(), vec![1]);
         assert_eq!(Vec::<i64>::from(label), [2]);
 
--- src/tokenization.rs
+++ src/tokenization.rs
@@ -2,6 +2,8 @@ use anyhow::Result;
 use rust_tokenizers::RobertaTokenizer;
 use std::path::Path;
 
+pub const PAD_TOKEN_ID: i64 = 1;
+
 pub fn load_tokenizer<P: AsRef<Path>>(model_resource_dir: P) -> Result<RobertaTokenizer> {
     let vocab_path = model_resource_dir.as_ref().join("vocab.txt");
     let merges_path = model_resource_dir.as_ref().join("merges.txt");
@@ -12,4 +14,19 @@ pub fn load_tokenizer<P: AsRef<Path>>(model_resource_dir: P) -> Result<RobertaTo
         false, // lowercase
         true,  // add prefix space
     )?)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rust_tokenizers::Tokenizer;
+
+    #[test]
+    fn test_tokenizer() {
+        let tokenizer = load_tokenizer("test_fixtures/tokenizer").unwrap();
+
+        // Make sure PAD_TOKEN_ID is correct.
+        let tokenized_pad = tokenizer.convert_tokens_to_ids(&tokenizer.tokenize("<pad>"));
+        assert_eq!(tokenized_pad, vec![PAD_TOKEN_ID]);
+    }
 }
